3eb6b71a60ea6245b6645c17dd5f1be8486933a4
Jang Haemin <io@jhaemin.com>
2021-11-01 21:32:45 +0900

    Expose port 3000

--- node/server.js
+++ node/server.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 
-const PORT = 8080
+const PORT = 3000
 const HOST = '0.0.0.0'
 
 // App
@@ -11,5 +11,6 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.listen(PORT, HOST)
-console.log(`Running on http://${HOST}:${PORT}`)
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`)
+})
